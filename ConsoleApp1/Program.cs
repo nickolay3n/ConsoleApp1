eb46f9301b53e6e09a7f82c00eda7ba72697f843
nickolay3n <nickolay3n@gmail.com>
2020-05-01 13:06:52 +0300

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string sentence = "This is another test";
+            string[] words =  sentence.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > 5)
+                {
+                    string reverse = "";
+                    for (int j = 0; j < (words[i].Length); j++)
+                    {
+                        reverse = reverse + words[i][words[i].Length -1 - j];
+                    }
+                    words[i] = reverse;
+                }
+            }
+            sentence = "";
+            for (int i = 0; i < words.Length; i++)
+                sentence = sentence + " "+words[i];
+            Console.WriteLine(sentence);
+        }
+    }
+}
